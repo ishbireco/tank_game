ef6011ad6378ca2f6cec6ce892576cda92dcbe13
Christopher Bejar <p.bejar.christopher@gmail.com>
2026-05-22 13:38:21 -0500

    update to keep coding style

--- shot.gd
+++ shot.gd
@@ -3,5 +3,5 @@ extends Area2D
 @export var speed = 100
 @onready var shot = $"."
 
-func _process(delta):
+func _process(delta: float) -> void:
     shot.position += Vector2.from_angle(rotation - deg_to_rad(90)) * speed * delta
